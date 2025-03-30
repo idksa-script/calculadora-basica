7e1486a7931f962bef6267d1efbafe315b7e7158
brian <brianarguello358@gmail.com>
2025-03-29 22:14:47 -0300

    base de la calculadora terminada

--- logicaCalculadora.py
+++ logicaCalculadora.py
@@ -3,3 +3,8 @@ def borrar(cuadro_texto):
 
 def agregar(cuadro_texto, t):
     cuadro_texto.insert("end", t)
+
+def calcular(cuadro_texto):
+    resultado = eval(cuadro_texto.get())
+    cuadro_texto.delete(0, "end")
+    cuadro_texto.insert("end", resultado)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk 
-from logicaCalculadora import borrar, agregar
+from logicaCalculadora import borrar, agregar, calcular
 #verifica la entrada del teclado asegurandose de que no sea letras solo numero y ciertos simnolos
 def validar_entrada(entrada):
     carateres = "1234567890.+-*/%"
@@ -28,7 +28,7 @@ frame_padre.pack(fill="both", expand=True)
 #frame hijo1 donde esta la entrada de texto
 frame_hijo1 = tk.Frame(frame_padre, bg="black")
 frame_hijo1.place(relx=0, rely=0, relwidth=1.0, relheight=0.35)
-cuadro_texto = tk.Entry(frame_hijo1, font=("Arial", 14), justify="right", validate="key", validatecommand=(validacion, "%P"), bg="black", fg="white", bd=0, highlightthickness=0)
+cuadro_texto = tk.Entry(frame_hijo1, font=("Monospace", 20), justify="right", validate="key", validatecommand=(validacion, "%P"), bg="black", fg="white", bd=0, highlightthickness=0)
 cuadro_texto.pack(fill="both", expand=True)
 
 ############
@@ -51,6 +51,10 @@ for i in range(4):
 for texto, filas, columnas in botones:
     if texto == "C":
         boton = tk.Button(frame_hijo2, text=texto, command=lambda: borrar(cuadro_texto))
+
+    elif texto == "=":
+        boton = tk.Button(frame_hijo2, text=texto, command=lambda: calcular(cuadro_texto))
+
     else:
         boton = tk.Button(frame_hijo2, text=texto, command= lambda t= texto: agregar(cuadro_texto, t))
     boton.grid(row=filas, column=columnas, sticky="nsew")
